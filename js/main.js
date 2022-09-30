27ba6dc8028cf9854e600bb4ca16c4560aea6af2
Ramses-Moreno <ram.ramses21sentinela@gmail.com>
2022-09-30 00:31:12 -0700

    Seccionar
    Partí la pagina en 3 para que los usuarios tengan mejor comodidad d la hora de hacer scroll.
    
    //aun las tarjetas no responden de manera responsiva ante el texto.

--- js/main.js
+++ js/main.js
@@ -19,3 +19,49 @@ const typed = new Typed('.typed', {
 	cursorChar: '|', // Caracter para el cursor
 	contentType: 'html', // 'html' o 'null' para texto sin formato
 });
+
+const typed2 = new Typed('.typed2', {
+	strings: [
+		'<i class="direct">Proyecto 1</i>',
+		'<i class="direct">Proyecto2</i>',
+		'<i class="direct">Proyecto3</i>',
+		'<i class="direct">Proyecto4</i>',
+		'<i class="direct">Proyecto5</i>'
+	],
+
+	//stringsElement: '#cadenas-texto', // ID del elemento que contiene cadenas de texto a mostrar.
+	typeSpeed: 60, // Velocidad en mlisegundos para poner una letra,
+	startDelay: 30, // Tiempo de retraso en iniciar la animacion. Aplica tambien cuando termina y vuelve a iniciar,
+	backSpeed: 60, // Velocidad en milisegundos para borrrar una letra,
+	smartBackspace: true, // Eliminar solamente las palabras que sean nuevas en una cadena de texto.
+	shuffle: false, // Alterar el orden en el que escribe las palabras.
+	backDelay: 1500, // Tiempo de espera despues de que termina de escribir una palabra.
+	loop: true, // Repetir el array de strings
+	loopCount: false, // Cantidad de veces a repetir el array.  false = infinite
+	showCursor: true, // Mostrar cursor palpitanto
+	cursorChar: '|', // Caracter para el cursor
+	contentType: 'html', // 'html' o 'null' para texto sin formato
+});
+
+const typed3 = new Typed('.typed3', {
+	strings: [
+		'<i class="direct">Linkedln</i>',
+		'<i class="direct">GitHub</i>',
+		'<i class="direct">Instagram</i>',
+		'<i class="direct">Twitter</i>',
+		'<i class="direct">Google Developers</i>'
+	],
+
+	//stringsElement: '#cadenas-texto', // ID del elemento que contiene cadenas de texto a mostrar.
+	typeSpeed: 60, // Velocidad en mlisegundos para poner una letra,
+	startDelay: 30, // Tiempo de retraso en iniciar la animacion. Aplica tambien cuando termina y vuelve a iniciar,
+	backSpeed: 60, // Velocidad en milisegundos para borrrar una letra,
+	smartBackspace: true, // Eliminar solamente las palabras que sean nuevas en una cadena de texto.
+	shuffle: false, // Alterar el orden en el que escribe las palabras.
+	backDelay: 1500, // Tiempo de espera despues de que termina de escribir una palabra.
+	loop: true, // Repetir el array de strings
+	loopCount: false, // Cantidad de veces a repetir el array.  false = infinite
+	showCursor: true, // Mostrar cursor palpitanto
+	cursorChar: '|', // Caracter para el cursor
+	contentType: 'html', // 'html' o 'null' para texto sin formato
+});
